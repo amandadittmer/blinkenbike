9b847b557fce8d4258847f03e41bd6b9c6656588
Jennifer Olid <jenniferolid@gmail.com>
2018-05-24 21:44:22 +0800

    Nyhedsbrev done

---
+++ nyhedsbrev-mail.php
@@ -0,0 +1,21 @@
+<?php
+
+$to=$_REQUEST['email'];
+
+//$navn=$_REQUEST['navn'];
+$email=$_REQUEST['email'];
+$emne="Velkommen til Blinkenbikes nyhedsbrev!";
+$besked="Du er nu tilmeldt Blinkenbikes nyhedsbrev. Vi glæder os til at holde dig opdateret om vores produkter.";
+
+$subject=$emne;
+$message="<h1>" . $emne . "</h1>" . $besked;
+
+
+$header = "Content-type: text/html; charset=utf-8" . "\r\n";
+$header.="from:jennifer@jenniferolid.dk";
+
+mail($to, $subject, $message, $header);
+header("Location: nyhedsbrev-tak.html");
+
+
+?>
