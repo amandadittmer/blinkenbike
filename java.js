9c21e6dfbf1d73b742766562523fa917e3e30cab
Jennifer Olid <jenniferolid@gmail.com>
2018-05-30 19:24:11 +0800

    Nyhedsbrev samt nyheder

--- java.js
+++ java.js
@@ -8,30 +8,6 @@ function myFunction() {
     }
 }
 
-// DESKTOP NAV
-document.addEventListener("DOMContentLoaded", headerHidden);
-
-function headerHidden() {
-    document.querySelector("#header").style.visibility = "hidden";
-}
-
-window.onscroll = function () {
-    headerScrollDown()
-};
-
-function headerScrollDown() {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-        document.querySelector("#header").style.visibility = "visible";
-        document.querySelector("#splash_logo").style.visibility = "hidden";
-        document.getElementById("header").className = "w3-animate-top";
-    } else {
-        document.querySelector("#header").style.visibility = "hidden";
-        document.querySelector("#splash_logo").style.visibility = "visible";
-
-    }
-}
-
-
 
 //// Get the modal
 //var modal = document.getElementById('myModal');
