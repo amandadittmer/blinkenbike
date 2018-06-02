189242825a785cc7cc66e5d57b7c8b61b32dd5ae
Jennifer Olid <jenniferolid@gmail.com>
2018-06-02 12:36:54 +0800

    Kontakt side done!

--- nav_scroll.js
+++ nav_scroll.js
@@ -2,8 +2,8 @@
 document.addEventListener("DOMContentLoaded", headerHidden);
 
 function headerHidden() {
-    document.querySelector("#header").style.visibility = "hidden";
-}
+    document.querySelector("#header").style.visibility = "hidden"
+};
 
 window.onscroll = function () {
     headerScrollDown()
@@ -11,11 +11,11 @@ window.onscroll = function () {
 
 function headerScrollDown() {
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-        document.querySelector("#header").style.visibility = "visible";
+        document.querySelector("#dekstop_nav").style.visibility = "visible";
         document.querySelector("#splash_logo").style.visibility = "hidden";
-        document.getElementById("header").className = "w3-animate-top";
+        document.getElementById("dekstop_nav").className = "w3-animate-top";
     } else {
-        document.querySelector("#header").style.visibility = "hidden";
+        document.querySelector("#dekstop_nav").style.visibility = "hidden";
         document.querySelector("#splash_logo").style.visibility = "visible";
 
     }
